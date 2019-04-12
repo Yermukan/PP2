266c221833dc542da372c98a6d50c3149633e1ce
Yermukan <vip.ermukhan@mail.ru>
2019-04-12 23:48:54 +0600

    added

---
+++ Week 9/Draw  the Image/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Draw__the_Image
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SeaShell;
+            this.label1.Location = new System.Drawing.Point(586, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Level: 1 Score: 200 Live: ***";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.DarkGreen;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Week 9/Draw  the Image/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draw__the_Image
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        Bitmap bitmap;
+        Point[] points;
+        Point[] trinpoints;
+        Point[] starpoints1;
+        Point[] starpoints_1;
+        Point[] starpoints2;
+        Point[] starpoints_2;
+        Point[] starpoints3;
+        Point[] starpoints_3;
+        Point[] starpoints4;
+        Point[] starpoints_4;
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+            points = new Point[] { new Point(300, 100), new Point(265, 125), new Point(265, 160), new Point(300, 185), new Point(335, 160), new Point(335, 125) };
+            trinpoints = new Point[] { new Point(300, 125), new Point(290, 142), new Point(310, 142) };
+            starpoints1 = new Point[]{new Point(80, 97), new Point(60, 127), new Point(100, 127)};
+            starpoints_1 = new Point[] { new Point(60, 108), new Point(100, 108), new Point(80, 137) };
+            starpoints2 = new Point[] { new Point(470, 92), new Point(450, 122), new Point(490, 122) };
+            starpoints_2 = new Point[] { new Point(450, 103), new Point(490, 103), new Point(470, 132) };
+            starpoints3 = new Point[] { new Point(100, 200), new Point(80, 230), new Point(120, 230) };
+            starpoints_3 = new Point[] { new Point(80, 211), new Point(120, 211), new Point(100, 240) };
+            starpoints4 = new Point[] { new Point(400, 250), new Point(380, 280), new Point(420, 280) };
+            starpoints_4 = new Point[] { new Point(380, 261), new Point(420, 261), new Point(400, 290) };
+            graphics.Clear(Color.DarkBlue);
+            SolidBrush solid = new SolidBrush(Color.White);
+            SolidBrush solid1 = new SolidBrush(Color.Yellow);
+            SolidBrush solid2 = new SolidBrush(Color.Green);
+            SolidBrush solid3 = new SolidBrush(Color.Red);
+            graphics.FillEllipse(solid, 15, 50, 20, 20);
+            graphics.FillEllipse(solid, 18, 300, 20, 20);
+            graphics.FillEllipse(solid, 200, 40, 20, 20);
+            graphics.FillEllipse(solid, 200, 250, 20, 20);
+            graphics.FillEllipse(solid, 350, 60, 20, 20);
+            graphics.FillEllipse(solid, 520, 150, 20, 20);
+            graphics.FillEllipse(solid, 470, 200, 20, 20);
+            graphics.FillEllipse(solid, 520, 300, 20, 20);
+            graphics.FillPolygon(solid1, points);
+            graphics.FillRectangle(solid2, 295, 142, 10, 20);
+            graphics.FillPolygon(solid2, trinpoints);
+            graphics.FillEllipse(solid2, 300, 75, 22, 7);
+            graphics.FillEllipse(solid2, 308, 67, 7, 22);
+            graphics.FillPolygon(solid3, starpoints1);
+            graphics.FillPolygon(solid3, starpoints_1);
+            graphics.FillPolygon(solid3, starpoints2);
+            graphics.FillPolygon(solid3, starpoints_2);
+            graphics.FillPolygon(solid3, starpoints3);
+            graphics.FillPolygon(solid3, starpoints_3);
+            graphics.FillPolygon(solid3, starpoints4);
+            graphics.FillPolygon(solid3, starpoints_4);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
